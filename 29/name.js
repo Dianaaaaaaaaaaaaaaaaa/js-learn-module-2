4ca5d43f255a6b81b1ac00db424ada21c487c17d
Diana <sergeeva.diana228@gmail.com>
2023-12-14 23:45:04 +0700

    Adding all labs

---
+++ 29/name.js
@@ -0,0 +1,23 @@
+const firstName = 'Диана';
+const lastName = 'Сергеева';
+const patronymic = 'Владимировна';
+const birthYear = 2001;
+const major = 'Прикладная информатика';
+
+
+const result1 = 'Имя: ' + firstName + '\n' +
+    'Фамилия: ' + lastName + '\n' +
+    'Отчество: ' + patronymic + '\n' +
+    'Год рождения: ' + birthYear + '\n' +
+    'Направление: ' + major;
+console.log(result1);
+
+console.log()
+
+const result2 = `Имя: ${firstName}
+Фамилия: ${lastName}
+Отчество: ${patronymic}
+Год рождения: ${birthYear}
+Направление: ${major}`;
+
+console.log(result2);
